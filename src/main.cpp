e837d9d32283f2ac44eab1bab8cc53ac42c23080
Julian <julian.camargo@sitrack.com>
2023-03-25 18:40:29 -0300

    lectura de ascii

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,12 @@
 #define AT_P2P_CONFIG_SET "AT+P2P=915000000:7:0:0:10:14"
 #define AT_P2P_CONFIG_GET "AT+P2P=?"
 #define AT_CONTINUOUS_PRECV_CONFIG_SET "AT+PRECV=65534" // 65534 for continuous receive, 65535 for continuous receive until one reception.
-#define AT_CONTINUOUS_PRECV_CONFIG_GET "AT+PRECV=?"
 
 bool initialized = false;
 WiFiManager wifiManager;
 String sendATCommand(String);
+String readSerial2();
+String hexToASCII(String);
 
 void setup() {
   Serial.begin(115200);
@@ -29,6 +30,17 @@ void loop() {
     return;
   } else {
     // Process received data
+    if(Serial2.available()>0) {
+      String rxData = readSerial2();
+      rxData = rxData.substring(rxData.lastIndexOf(':')+1);
+      rxData.trim();
+      
+      Serial.print("Received: ");
+      Serial.println(rxData);
+      // Convert payload from hex to ascii
+      Serial.print("ASCII: ");
+      Serial.println(hexToASCII(rxData));
+    }
   }
 }
 
@@ -43,14 +55,30 @@ String sendATCommand(String command) {
   Serial2.println(command);
   delay(500);
   Serial2.flush();
-  while(Serial2.available()>0) {
-    char c = Serial2.read();
-    Serial.print(c);
-    response += c;
-  }
+  response = readSerial2();
   if( !configCommand ) {
     response = response.substring(0, response.indexOf('\r'));
   }
   response.trim();
   return response;
+}
+
+String readSerial2() {
+  String readed = "";
+  while(Serial2.available()>0) {
+    char c = Serial2.read();
+    Serial.print(c);
+    readed += c;
+  }
+  return readed;
+}
+
+String hexToASCII(String hex) {
+  String ascii = "";
+  for(int i=0; i<hex.length(); i+=2) {
+    String ch = hex.substring(i, i+2);
+    char c = (char) (int)strtol(ch.c_str(), NULL, HEX);
+    ascii += (char) c;
+  }
+  return ascii;
 }
