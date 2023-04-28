f0cd391f87308de07b53327391b9ac09b3b405ec
Julian <julian.camargo@sitrack.com>
2023-04-27 21:34:35 -0300

    subida a db con datos mock

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <WiFiManager.h>
 #include <NTPClient.h>
+#include <HTTPClient.h>
 #include <Utils.h>
 
 #define AT_CONTINUOUS_PRECV_CONFIG_SET "AT+PRECV=65534" // 65534 for continuous receive, 65535 for continuous receive until one reception.
@@ -11,6 +12,7 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, POOL_NTP_URL, -utcOffsetInSeconds);
 bool initialized = false;
 WiFiManager wifiManager;
+HTTPClient http;
 
 String getLocalTimeStamp(NTPClient timeClient) {
   timeClient.update();
@@ -33,24 +35,43 @@ String getLocalTimeStamp(NTPClient timeClient) {
   return timeString;
 }
 
+int parseRxData(){
+  String currentTime = getLocalTimeStamp(timeClient);
+  String sqlTemplate = "{\"stmt\": \"INSERT INTO spa.weatherstation (timestamp, windSpeed, windDirection, humidity, radiation, temperature, pressure, leafMoisture, pluviometer) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) \",\"args\":";
+  char buffer[100]; 
+
+  sprintf(buffer, "[\"%s\", %d, %d, %d, %d, %d, %d, %d, %d]}", currentTime.c_str(), 60, 90, 80, 65, 33, 68, 88, 25);
+
+  String finalData = sqlTemplate + buffer;
+  
+  int httpResponseCode = http.PUT(finalData);
+  return httpResponseCode;
+}
+
 void setup() {
   Serial.begin(115200);
-  Serial2.begin(115200);
+  // Serial2.begin(115200);
   wifiManager.autoConnect();
-  String atCommandResetResponse = sendATCommand(Serial2, AT_RESET);
-  String atConfigSetP2PResponse = sendATCommand(Serial2, AT_P2P_CONFIG_SET);
-  String atConfigGetP2PResponse = sendATCommand(Serial2, AT_P2P_CONFIG_GET);
-  String atConfigContRecvResponse = sendATCommand(Serial2, AT_CONTINUOUS_PRECV_CONFIG_SET);
+  // String atCommandResetResponse = sendATCommand(Serial2, AT_RESET);
+  // String atConfigSetP2PResponse = sendATCommand(Serial2, AT_P2P_CONFIG_SET);
+  // String atConfigGetP2PResponse = sendATCommand(Serial2, AT_P2P_CONFIG_GET);
+  // String atConfigContRecvResponse = sendATCommand(Serial2, AT_CONTINUOUS_PRECV_CONFIG_SET);
 }
 
 void loop() {
-      String rxData = readSerial(Serial2);
-      rxData.trim();
-      rxData = hexToASCII(rxData.substring(rxData.lastIndexOf(':')+1));
-      Serial.print("Received: ");
-      Serial.println(rxData);
-      
-      delay(1000);
-      String currentTime = getLocalTimeStamp(timeClient);
-      
+  // String rxData = readSerial(Serial2);
+  // rxData.trim();
+  // rxData = hexToASCII(rxData.substring(rxData.lastIndexOf(':')+1));
+  // Serial.print("Received: ");
+  // Serial.println(rxData);
+
+  http.begin("https://spautncluster.aks1.eastus2.azure.cratedb.net:4200/_sql");
+  http.addHeader("Content-Type", "application/json");
+  http.setAuthorization("admin", "BmY8bcMNbCgrsHDBmY8bcMNbCgrsHD");
+
+  Serial.print("RESPONSE: ");
+  Serial.println(parseRxData());
+  
+  delay(3000);
+  
 }
