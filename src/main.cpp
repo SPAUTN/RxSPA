af55b9059411256c662bf1c97667ff330ef07246
Julian <julian.camargo@sitrack.com>
2023-05-05 22:36:19 -0300

    added poll method

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <Arduino.h>
 #include <WiFiManager.h>
-#include <NTPClient.h>
 #include <HTTPClient.h>
 #include <Utils.h>
+#include <esp_sntp.h>
+#include <time.h>
 #include <esp_random.h>
 
 #define AT_CONTINUOUS_PRECV_CONFIG_SET "AT+PRECV=65534" // 65534 for continuous receive, 65535 for continuous receive until one reception.
+#define AT_SEMICONTINUOUS_PRECV_CONFIG_SET "AT+PRECV=65535" 
 #define utcOffsetInSeconds 10800
 #define POOL_NTP_URL "pool.ntp.org"
 
@@ -14,22 +16,20 @@
 #define DB_PASS "Spautn2023pf"
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, POOL_NTP_URL);
-bool initialized = false;
 WiFiManager wifiManager;
 HTTPClient http;
+String sendedHour = "xx";
+int sendedMinutes = 0;
 
-String getLocalTimeStamp(NTPClient timeClient) {
-  timeClient.update();
-  time_t currentTime = timeClient.getEpochTime();
-
-  // Verificar si el valor de tiempo Unix es coherente
-  while (currentTime < 1682646677 || currentTime > 2000000000) {
-    timeClient.forceUpdate();
-    currentTime = timeClient.getEpochTime();
-  }
+String getLocalTimeStamp() {
+  time_t now;
   struct tm timeInfo;
-  localtime_r(&currentTime, &timeInfo);
+  char strftime_buf[64];
+
+  time(&now);
+  localtime_r(&now, &timeInfo);
+  
+  //localtime_r(&currentTime, &timeInfo);
   
   char timeString[30];
   snprintf(timeString, sizeof(timeString), "%04d-%02d-%02dT%02d:%02d:%02d%", 
@@ -43,11 +43,27 @@ int parseRxData(int windSpeed, int windDirection, long int humidity, long int ra
   http.begin(DB_HOST);
   http.addHeader("Content-Type", "application/json");
   http.setAuthorization("admin", "BmY8bcMNbCgrsHDBmY8bcMNbCgrsHD");
-  String currentTime = getLocalTimeStamp(timeClient);
+  String currentTime = getLocalTimeStamp();
   String sqlTemplate = "{\"stmt\": \"INSERT INTO spa.weatherstation (timestamp, windSpeed, windDirection, humidity, radiation, temperature, pressure, leafMoisture, pluviometer, weight) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) \",\"args\":";
   char buffer[100]; 
 
   sprintf(buffer, "[\"%s\", %d, %d, %d, %d, %d, %d, %d, %d, %d]}", currentTime.c_str(), windSpeed, windDirection, humidity, radiation, temperature, pressure, leafMoisture, pluviometer, weight);
+
+  String finalData = sqlTemplate + buffer;
+  
+  int httpResponseCode = http.PUT(finalData);
+  http.end();
+  return httpResponseCode;
+}
+
+int logWrite(String timestamp, int httpCode){
+  http.begin(DB_HOST);
+  http.addHeader("Content-Type", "application/json");
+  http.setAuthorization("admin", "BmY8bcMNbCgrsHDBmY8bcMNbCgrsHD");
+  String sqlTemplate = "{\"stmt\": \"INSERT INTO spa.logs (timestamp, httpCode) VALUES ($1, $2) \",\"args\":";
+  char buffer[100]; 
+
+  sprintf(buffer, "[\"%s\", %d]}", timestamp.c_str(), httpCode);
 
   String finalData = sqlTemplate + buffer;
   
@@ -63,26 +79,73 @@ void setup() {
   String atCommandResetResponse = sendATCommand(Serial2, AT_RESET);
   String atConfigSetP2PResponse = sendATCommand(Serial2, AT_P2P_CONFIG_SET);
   String atConfigGetP2PResponse = sendATCommand(Serial2, AT_P2P_CONFIG_GET);
-  String atConfigContRecvResponse = sendATCommand(Serial2, AT_CONTINUOUS_PRECV_CONFIG_SET);
+  String atConfigContRecvResponse = sendATCommand(Serial2, AT_P2P_CONFIG_TX_SET);
 }
 
 void loop() {
-  String rxData = readSerial(Serial2);
-  rxData.trim();
-  rxData = hexToASCII(rxData.substring(rxData.lastIndexOf(':')+1));
-  Serial.print("Received: ");
-  Serial.println(rxData);
+  // if(Serial2.available()) {
+  //   String rxData = readSerial(Serial2);
+  //   rxData.trim();
+  //   rxData = hexToASCII(rxData.substring(rxData.lastIndexOf(':')+1));
+  //   Serial.print("Received: ");
+  //   Serial.println(rxData);
+  // }
 
-  //ParseData
+  int httpResponse;
+  String currentTime = getLocalTimeStamp();
+  Serial.print("Current time: ");
+  Serial.println(currentTime);
+  String hour = currentTime.substring(11,13);
+  int minutes = atoi(currentTime.substring(14,16).c_str());
 
-  // SendData
-  //int httpResponseCode = parseRxData(windSpeed, windDirection, humidity, radiation, temperature, pressure, leafMoisture, pluviometer, weight);
-  
-  // Get httpResponseCode and make decision
-  
-  // Only for Test
-  // Serial.print("RESPONSE: ");
-  // Serial.println(parseRxData(esp_random() % 100, esp_random() % 100, esp_random() % 100, esp_random() % 100, esp_random() % 100, esp_random() % 100, esp_random() % 100, esp_random() % 100, esp_random() % 100));
-  // delay(500);
-  
+  if(minutes % 2 == 0) {
+    if(sendedMinutes != minutes){
+      Serial.println("Petición de datos a estación.");
+
+      String pollResponse = sendP2PPacket(Serial2, asciiToHex("POLL"));
+      String listeningResponse = sendATCommand(Serial2, AT_SEMICONTINUOUS_PRECV_CONFIG_SET);
+
+      if (Serial2.available()) {
+        String rxData = readSerial(Serial2);
+        rxData.trim();
+        rxData = hexToASCII(rxData.substring(rxData.lastIndexOf(':')+1));
+        Serial.print("DATOS RECIBIDOS: ");
+        Serial.println(rxData);
+      }
+
+      do {
+        httpResponse = parseRxData(esp_random() % 100, esp_random() % 100, esp_random() % 100, esp_random() % 100, esp_random() % 100, esp_random() % 100, esp_random() % 100, esp_random() % 100, esp_random() % 100);
+        // Serial.print("RESPONSE: ");
+        // Serial.println(httpResponse);
+        // Serial.print("TIME: ");
+        // Serial.print(currentTime);
+
+        // log table writing
+        logWrite(currentTime, httpResponse);
+        // Serial.print("Last sendedHour: ");
+        // Serial.println(sendedHour);
+        
+        Serial.print("Last sendedMinutes: ");
+        Serial.println(sendedMinutes);
+        sendedHour = hour;
+        sendedMinutes = minutes;
+        Serial.print(currentTime);
+        Serial.print(" -> ");
+        Serial.println(httpResponse);
+        // Serial.print("New sendedHour: ");
+        // Serial.println(sendedHour);
+        
+        Serial.print("New sendedMinutes: ");
+        Serial.println(sendedMinutes);
+      } while (httpResponse != 200);
+    } else {
+        Serial.print("Mismo minuto, no se envían datos. Timestamp: ");
+        Serial.print(currentTime);
+        Serial.print(" - ");
+        Serial.println(sendedMinutes);
+    }
+  } else {
+      Serial.print("Minuto no par, no se envían datos. Timestamp: ");
+      Serial.println(currentTime);
+  }
 }
