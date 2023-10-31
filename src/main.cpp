ef1598620c27ca0c9a06dfe8ac814afce05d5e3f
Julian <julian.camargo@sitrack.com>
2023-10-31 11:36:15 -0300

    fix: logger frame

--- src/main.cpp
+++ src/main.cpp
@@ -175,7 +175,7 @@ void loop() {
           Serial.println(rxData);
           frameReceived = true;
           frame = rxData;
-          logger(0, "Received frame data from SPA: " + frame, INFO_LEVEL);
+          logger(0, "Received frame data from SPA: '" + frame + "'", INFO_LEVEL);
         }
         if (actualMilis + timeToAttempt <= millis()) {
           Serial.printf("\nResending %s command...", &pollCommand);
