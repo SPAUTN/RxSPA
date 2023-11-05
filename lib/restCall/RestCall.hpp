f6a7af16197dfce0894d75087151c700e1714771
Julian <julian.camargo@sitrack.com>
2023-11-05 19:03:24 -0300

    fix: error by name and getter & setters

---
+++ lib/restCall/RestCall.hpp
@@ -0,0 +1,85 @@
+#include <Arduino.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include <Logger.hpp>
+
+#define STATION_TABLE "spa.weatherstation"
+#define WET_WEIGHT_TABLE "spa.wetweights"
+#define ETC_TABLE "spa.etc"
+
+#define INSERT_CONTEXT "/insert"
+#define LOG_CONTEXT "/log"
+#define ETCRAIN_CONTEXT "/etcrain"
+
+#define ERROR_LEVEL "ERROR"
+#define INFO_LEVEL "INFORMATION"
+#define DEBUG_LEVEL "DEBUG"
+
+class RestCall {
+    public:
+
+        /**
+         * @brief Configure the rest call
+         * 
+         * @param dbHost The host to send the data
+         * @param dbUser The user to authenticate on the host
+         * @param dbPass The password to authenticate on the host
+        */
+        void config(String dbHost, String dbUser, String dbPass);        
+        
+        /**
+         * @brief Get the weight and rain values from the backend
+         * 
+         * @param command The command to send to the backend
+         * @return String The response of the backend
+        */
+        String getWeightAndRain(String command);
+        
+        /**
+         * @brief Send a frame to the backend
+         * 
+         * @param frame The frame to send
+         * @param table The table to insert the frame
+         * @param attempts The number of attempts to send the frame
+         * @return String The response of the backend
+        */
+        String sendFrameData(String frame, String table, int attempts);
+
+        /**
+         * @brief Set the debug level
+         * 
+         * @param debugLevel The debug level to set
+         * @return String The debug level
+        */
+        void setDebugLevel(String debugLevel);
+
+        /**
+         * @brief Get the debug level
+         * 
+         * @return String The debug level
+        */
+        String getDebugLevel();
+
+        /**
+         * @brief Set the response code
+         * 
+         * @param responseCode The response code to set
+         * @return int The response code
+        */
+        void setResponseCode(int responseCode);
+
+        /**
+         * @brief Get the response code
+         * 
+         * @return int The response code
+        */
+        int getResponseCode();
+
+    private:
+        String dbHost;
+        String dbUser;
+        String dbPass;
+        HTTPClient http;
+        String debugLevel;
+        int responseCode;
+};
