77d4119b61fcadfa8050c0476135a2db83048214
Julian <julian.camargo@sitrack.com>
2023-11-05 19:04:00 -0300

    refactor: use of logger and restCall

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <time.h>
 #include <esp_random.h>
 #include <Logger.hpp>
+#include <RestCall.hpp>
 
 #define AT_CONTINUOUS_PRECV_CONFIG_SET "AT+PRECV=65534" // 65534 for continuous receive, 65535 for continuous receive until one reception.
 #define AT_SEMICONTINUOUS_PRECV_CONFIG_SET "AT+PRECV=65535" 
@@ -21,10 +22,6 @@
 #define DB_USER "serviceesp"
 #define DB_PASS "Spautn2023pf"
 
-#define STATION_TABLE "spa.weatherstation"
-#define WET_WEIGHT_TABLE "spa.wetweights"
-#define ETC_TABLE "spa.etc"
-
 WiFiUDP ntpUDP;
 WiFiManager wifiManager;
 HTTPClient http;
@@ -32,6 +29,7 @@ String sendedHour = "xx";
 int sendedMinutes = 0;
 
 Logger logger;
+RestCall restCall;
 
 String getLocalTimeStamp() {
   time_t now;
@@ -47,67 +45,9 @@ String getLocalTimeStamp() {
     timeInfo.tm_hour, timeInfo.tm_min, timeInfo.tm_sec);
 
   logger.config(String(LOG_HOST), String(DB_USER), String(DB_PASS));
+  restCall.config(String(DB_HOST), String(DB_USER), String(DB_PASS));
   
   return timeString;
-}
-
-int sendFrameData(String frame, String table, int attempts){
-  int n_attemp = 0;
-  Serial.print("Frame to send: ");
-  Serial.println(frame);
-  http.begin(DB_HOST);
-  http.addHeader("Content-Type", "application/json");
-  http.setAuthorization(DB_USER, DB_PASS);
-  String bodyRequest = "{\"table\": \"" + table + "\",\"frame\": " + frame + "}";
-  Serial.print("Body request: ");
-  int httpResponseCode;
-  String log_message;
-  do {
-    n_attemp ++;
-    Serial.println(bodyRequest);
-    httpResponseCode = http.POST(bodyRequest);
-    String httpMessage = http.getString();
-    httpMessage = httpMessage.substring(12, httpMessage.length()-2);
-    log_message = "Inserting on table: " + table + " - http message: <" + httpMessage + "> - on inserting frame";
-    Serial.print("HTTP Response code: ");
-    Serial.print(httpResponseCode);
-    Serial.print(" on attemp number ");
-    Serial.println(n_attemp);
-    if(httpResponseCode != 201) {
-      logger.log(httpResponseCode, log_message, ERROR_LEVEL);
-      delay(500);
-    } else {
-      logger.log(httpResponseCode, log_message, DEBUG_LEVEL);
-    }
-  } while (n_attemp > attempts && httpResponseCode != 201);
-  
-  http.end();
-
-  return httpResponseCode;
-}
-
-String queryWetweightAndRainValues(String command = "") {
-  HTTPClient http;
-  String wetweightAndRainValues = "";
-  http.begin(ETCRAIN_HOST);
-  http.addHeader("Content-Type", "application/json");
-  http.setAuthorization(DB_USER, DB_PASS);
-  int httpCode = http.GET();
-
-  if (httpCode == 200) {
-    String responseBody = http.getString();
-    http.end();
-    Serial.print("Query received: ");
-    Serial.println(responseBody);
-    const size_t capacity = JSON_OBJECT_SIZE(2) + 40;
-    DynamicJsonDocument doc(capacity);
-    deserializeJson(doc, responseBody);
-    double wetweight = doc["wetweight"];
-    double cumulative_rain = doc["cumulative_rain"];
-    wetweightAndRainValues = String(wetweight, 2) + ";" + String(cumulative_rain, 2);
-    Serial.println(wetweightAndRainValues);
-  }
-  return command + ";" + wetweightAndRainValues + ";";
 }
 
 void setup() {
@@ -128,7 +68,7 @@ void setup() {
 
 void loop() {
   String pollCommand;
-  int httpResponse;
+  String restCallResponse;
   int timeToAttempt = 0;
   String currentTime = getLocalTimeStamp();
   String hour = currentTime.substring(11,13);
@@ -140,13 +80,13 @@ void loop() {
     if(sendedHour != hour){
       Serial.println("Polling to SPA...");
       if(currentTime.substring(11,19) == "00:00:00"){
-        pollCommand = queryWetweightAndRainValues(String(IRR_COMMAND));
+        pollCommand = restCall.getWeightAndRain(String(IRR_COMMAND));
         timeToAttempt = 60000;
       } else {
         pollCommand = POLL_COMMAND;
         timeToAttempt = 15000;
       }    
-      logger.log(0, "Sending " + pollCommand + " to SPA.", INFO_LEVEL);
+      logger.info(0, "Sending " + pollCommand + " to SPA.");
       String pollResponse = sendP2PPacket(Serial2, pollCommand);
       String listeningResponse = sendATCommand(Serial2, AT_SEMICONTINUOUS_PRECV_CONFIG_SET);
       boolean frameReceived = false;
@@ -165,11 +105,11 @@ void loop() {
           frame = rxData;
           String frameLog = frame;
           frameLog.replace("\"", "'");
-          logger.log(0, "Received frame data from SPA: '" + frameLog + "'", INFO_LEVEL);
+          logger.info(0, "Received frame data from SPA: '" + frameLog + "'");
         }
         if (actualMilis + timeToAttempt <= millis()) {
           Serial.printf("\nResending %s command...", &pollCommand);
-          logger.log(0, "Not frame received, resending command " + pollCommand + " to SPA.", ERROR_LEVEL);
+          logger.error(0, "Not frame received, resending command " + pollCommand + " to SPA.")
           sendATCommand(Serial2, AT_P2P_CONFIG_TX_SET);
           sendP2PPacket(Serial2, pollCommand);
           sendATCommand(Serial2, AT_SEMICONTINUOUS_PRECV_CONFIG_SET);
@@ -178,11 +118,17 @@ void loop() {
       }
       
       if(!pollCommand.startsWith(IRR_COMMAND)) {
-        httpResponse = sendFrameData(frame, STATION_TABLE, 3);
+        restCallResponse = restCall.sendFrameData(frame, STATION_TABLE, 3);
+        logger.log(restCall.getResponseCode(), restCallResponse, restCall.getDebugLevel());
       } else {
-        httpResponse = sendFrameData(frame.substring(0, frame.indexOf("etc")-2) + "}", STATION_TABLE, 3);
-        httpResponse = sendFrameData("{" + frame.substring(frame.indexOf("etc")-1, frame.indexOf("wetweight")-2) + "}", ETC_TABLE, 3);
-        httpResponse = sendFrameData("{" + frame.substring(frame.indexOf("wetweight")-1, frame.length()), WET_WEIGHT_TABLE, 3);
+        restCallResponse = restCall.sendFrameData(frame.substring(0, frame.indexOf("etc")-2) + "}", STATION_TABLE, 3);
+        logger.log(restCall.getResponseCode(), restCallResponse, restCall.getDebugLevel());
+        
+        restCallResponse = restCall.sendFrameData("{" + frame.substring(frame.indexOf("etc")-1, frame.indexOf("wetweight")-2) + "}", ETC_TABLE, 3);
+        logger.log(restCall.getResponseCode(), restCallResponse, restCall.getDebugLevel());
+        
+        restCallResponse = restCall.sendFrameData("{" + frame.substring(frame.indexOf("wetweight")-1, frame.length()), WET_WEIGHT_TABLE, 3);
+        logger.log(restCall.getResponseCode(), restCallResponse, restCall.getDebugLevel());
       }
       sendedHour = hour;
       sendedMinutes = minutes;
