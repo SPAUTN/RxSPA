02a5a5817d5fab3c98056be3afe4de36df47cabd
Julian <julian.camargo@sitrack.com>
2023-11-10 16:34:41 -0300

    chore: refactor parse frame received from adc

--- src/main.cpp
+++ src/main.cpp
@@ -105,13 +105,13 @@ void loop() {
           restCallResponse = restCall.sendFrameData(frame, STATION_TABLE, 3);
           logger.log(restCall.getResponseCode(), restCallResponse, restCall.getDebugLevel(), RXSPA);
         } else {
-          restCallResponse = restCall.sendFrameData(frame.substring(0, frame.indexOf(ETC)-2) + "}", STATION_TABLE, 3);
+          restCallResponse = restCall.sendFrameData(String(IRR_COMMAND) + frame.substring(0, frame.indexOf(ETC)-1), STATION_TABLE, 3);
           logger.log(restCall.getResponseCode(), restCallResponse, restCall.getDebugLevel(), RXSPA);
           
-          restCallResponse = restCall.sendFrameData("{" + frame.substring(frame.indexOf(ETC)-1, frame.indexOf(WET_WEIGHT)-2) + "}", ETC_TABLE, 3);
+          restCallResponse = restCall.sendFrameData(String(IRR_COMMAND) + frame.substring(frame.indexOf(ETC)-1, frame.indexOf(WET_WEIGHT)-1) , ETC_TABLE, 3);
           logger.log(restCall.getResponseCode(), restCallResponse, restCall.getDebugLevel(), RXSPA);
           
-          restCallResponse = restCall.sendFrameData("{" + frame.substring(frame.indexOf(WET_WEIGHT)-1, frame.length()), WET_WEIGHT_TABLE, 3);
+          restCallResponse = restCall.sendFrameData(String(IRR_COMMAND) + frame.substring(frame.indexOf(WET_WEIGHT)-1, frame.length()), WET_WEIGHT_TABLE, 3);
           logger.log(restCall.getResponseCode(), restCallResponse, restCall.getDebugLevel(), RXSPA);
         }
         sendedHour = hour;
